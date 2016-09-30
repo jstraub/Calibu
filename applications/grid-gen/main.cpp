deb2428a066de5a3092f60132572f3d80f50ca2b
Julian Straub <jstraub@csail.mit.edu>
2016-09-29 20:29:18 -0400

    grid-gen for seed 76 row 16 and col 12 target

--- applications/grid-gen/main.cpp
+++ applications/grid-gen/main.cpp
@@ -18,13 +18,13 @@ void UserQuit(int)
     should_run = false;
 }
 
-int main( int, char** )
+int main( int argc, char** argv )
 {
     signal(SIGABRT,UserQuit);
     signal(SIGTERM,UserQuit);
 
-    const int PR = 24;//35;//10;
-    const int PC = 12;//35;//10;
+    const int PR = 16;//24;//35;//10;
+    const int PC = 12;//12;//35;//10;
 
     uint32_t seed = FindBestSeed(PR, PC, should_run); // 14
     //uint32_t seed = 62;
@@ -37,8 +37,9 @@ int main( int, char** )
     const int auto_corr = AutoCorrelation(PG,5,5);
     std::cout << "Bad auto correlation matches: " << auto_corr << std::endl;
 
-    //SaveEPS("target.eps", PG[0], Eigen::Vector2d(0,0), 20.0, 4.0, 6.0, 4.0, seed);
-    SaveEPS("target.eps", PG[0], Eigen::Vector2d(0,0), 60.0, 12.0, 18.0, 4.0, seed);
+    SaveEPS("target.eps", PG[0], Eigen::Vector2d(0,0), 15.0, 4.0, 6.0, 4.0, seed);
+    // large
+    //SaveEPS("target.eps", PG[0], Eigen::Vector2d(0,0), 60.0, 12.0, 18.0, 4.0, seed);
 
     return 0;
 }
